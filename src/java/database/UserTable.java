5be1c5dba21b083e49acd7c2752d9bf204101a3e
Ismail Hassan <ismail.bukittinggi@gmail.com>
2016-05-09 01:15:34 +0700

    Menambahkan controller untuk profile dan follow

--- src/java/database/UserTable.java
+++ src/java/database/UserTable.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller;
+package database;
 
 /**
  *
  * @author ismail.hassan
  */
-public class StatusController {
+public class UserTable {
     
 }
